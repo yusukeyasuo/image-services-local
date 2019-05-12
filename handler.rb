51237417a57ec8628024052406b9a820dbece310
Yusuke Yasuo <yasuo.y@dely.jp>
2019-05-12 22:24:15 +0900

    feat: scraping mercari item page by nokogiri

--- handler.rb
+++ handler.rb
@@ -2,10 +2,40 @@ require 'json'
 require 'open-uri'
 require 'mini_magick'
 require 'net/http'
+require 'nokogiri'
 
-def download_image
+def get_image_urls(url)
+  puts "[start] get_image_urls"
+  charset = 'utf-8'
+
+  html = open(url) do |f|
+    charset = f.charset
+    f.read
+  end
+
+  doc = Nokogiri::HTML.parse(html, nil, charset)
+
+  image_urls = []
+  if url.match(/\Ahttps:\/\/item.mercari.com/)
+    image_urls = get_mercari_image_urls(doc)
+  end
+
+  image_urls.each do |image_url|
+    download_image(image_url)
+  end
+end
+
+def get_mercari_image_urls(doc)
+  image_urls = []
+  doc.xpath('//img[@class="owl-lazy"]/@data-src').each do |src|
+    image_urls << src
+  end
+  image_urls
+end
+
+def download_image(image_url)
   File.open('tmp/tmp.jpg', 'wb') do |file|
-    open('https://img.fril.jp/img/214847566/l/611236820.jpg?1553352512') do |img|
+    open(image_url) do |img|
       file.puts img.read
     end
   end
@@ -19,4 +49,5 @@ def resize_to_850
   image.write 'tmp/output.jpg'
 end
 
-download_image
+item_url = "https://item.mercari.com/jp/m62006909909/?_s=U2FsdGVkX1_IdRU8RQq0HWNkoCWRzLYt1jUnrtVk24Bqb5z5jXS9-e-x6yCRu1pdnbUCAMXmkSi-_c66xs6GSreOo4Azj29kfrGdJDfvo2nLZEKpyE5ElGFRjeWhO4DL"
+get_image_urls(item_url)
