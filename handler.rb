59812d23966d518805e9c932f4abab626f8888be
Yusuke Yasuo <yasuo.y@dely.jp>
2019-05-13 08:22:18 +0900

    feat: add method get_fril_image_urls

--- handler.rb
+++ handler.rb
@@ -3,6 +3,7 @@ require 'open-uri'
 require 'mini_magick'
 require 'net/http'
 require 'nokogiri'
+require 'aws-sdk-s3'
 
 def get_image_urls(url)
   puts "[start] get_image_urls"
@@ -15,10 +16,13 @@ def get_image_urls(url)
 
   doc = Nokogiri::HTML.parse(html, nil, charset)
 
-  image_urls = []
-  if url.match(/\Ahttps:\/\/item.mercari.com/)
-    image_urls = get_mercari_image_urls(doc)
-  end
+  image_urls = if url.match(/\Ahttps:\/\/item.mercari.com/)
+                 get_mercari_image_urls(doc)
+               elsif url.match(/\Ahttps:\/\/item.fril.jp/)
+                 get_fril_image_urls(doc)
+               else
+                 []
+               end
 
   image_urls.each do |image_url|
     download_image(image_url)
@@ -28,6 +32,16 @@ end
 def get_mercari_image_urls(doc)
   image_urls = []
   doc.xpath('//img[@class="owl-lazy"]/@data-src').each do |src|
+    image_urls << src
+  end
+  image_urls
+end
+
+def get_fril_image_urls(doc)
+  puts "[start] get_fril_image_urls"
+  image_urls = []
+  doc.xpath('//img[@class="sp-image"]/@src').each do |src|
+    puts src
     image_urls << src
   end
   image_urls
@@ -49,5 +63,6 @@ def resize_to_850
   image.write 'tmp/output.jpg'
 end
 
-item_url = "https://item.mercari.com/jp/m62006909909/?_s=U2FsdGVkX1_IdRU8RQq0HWNkoCWRzLYt1jUnrtVk24Bqb5z5jXS9-e-x6yCRu1pdnbUCAMXmkSi-_c66xs6GSreOo4Azj29kfrGdJDfvo2nLZEKpyE5ElGFRjeWhO4DL"
+#item_url = "https://item.mercari.com/jp/m62006909909/?_s=U2FsdGVkX1_IdRU8RQq0HWNkoCWRzLYt1jUnrtVk24Bqb5z5jXS9-e-x6yCRu1pdnbUCAMXmkSi-_c66xs6GSreOo4Azj29kfrGdJDfvo2nLZEKpyE5ElGFRjeWhO4DL"
+item_url = "https://item.fril.jp/750029bbf335d74fbc730a285ff36468"
 get_image_urls(item_url)
